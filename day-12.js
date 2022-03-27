8c671115ed3ea37c9b09950030d5d72b25ecdbd1
unknown <mksunnybd@gmail.com>
2022-03-27 17:04:18 +0600

    day-12-modify

--- day-12.js
+++ day-12.js
@@ -1,4 +1,15 @@
-function main() {
+// function main() {
+//     const PI = Math.PI;
+//     let r = 2.6; // let r = readLine();
+//     let area = PI * r * r;
+//     console.log(area);
+//     let perimeter = 2 * PI * r;
+//     console.log(perimeter);
+// }
+
+// main();
+
+let main = () => {
     const PI = Math.PI;
     let r = 2.6; // let r = readLine();
     let area = PI * r * r;
