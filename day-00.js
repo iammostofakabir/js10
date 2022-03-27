bba3aa51695f524eea1057fadfc5909e6b562e77
unknown <mksunnybd@gmail.com>
2022-03-27 15:29:04 +0600

    day-00

---
+++ day-00.js
@@ -0,0 +1,8 @@
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+}
+
+let parameterVariable = 'Welcome to 10 Days of JavaScript!';
+
+greeting(parameterVariable);
