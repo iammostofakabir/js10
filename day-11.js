ab72d61f80947637ffb4819bdd12ba3237f9c466
unknown <mksunnybd@gmail.com>
2022-03-27 16:22:58 +0600

    day-11-modify

--- day-11.js
+++ day-11.js
@@ -1,12 +1,25 @@
-function factorial(n) {
-    let f = 1;
-    while (n >= 1) {
-        f = f * n;
-        n = n - 1;
+// function factorial(n) {
+//     let f = 1;
+//     while (n >= 1) {
+//         f = f * n;
+//         n = n - 1;
+//     }
+//     return f;
+// }
+
+// let ans = 4;
+
+// console.log(factorial(ans));
+
+let factorial = n => {
+        let f = 1;
+        while (n >= 1) {
+            f = f * n;
+            n = n - 1;
+        }
+        return f;
     }
-    return f;
-}
-
+    
 let ans = 4;
-
+    
 console.log(factorial(ans));
